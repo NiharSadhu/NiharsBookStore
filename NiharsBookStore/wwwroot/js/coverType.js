b756e85e86761ecd4aacc52f9a07acfba2ad42f6
NiharSadhu <W0810177@myscc.ca>
2023-04-20 03:38:48 -0400

    fixing few errors in the js files

--- NiharsBookStore/wwwroot/js/coverType.js
+++ NiharsBookStore/wwwroot/js/coverType.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     loadDataTable();
 });
 
+
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
@@ -15,15 +16,15 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `
-                        <div class="text-center">
-                            <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
-                                <i class="fas fa-edit"></i>
-                            </a>
-                            <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
-                                <i class="fas fa-trash-alt"></i>
-                            </a>
-                        </div>
-                    `;
+                            <div class="text-center">
+                                <a href="/Admin/CoverType/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>&nbsp;
+                                </a>
+                                <a onclick=Delete("/Admin/CoverType/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i>&nbsp;
+                                </a>
+                            </div>
+                            `;
                 }, "width": "40%"
             }
         ]
@@ -32,7 +33,7 @@ function loadDataTable() {
 
 function Delete(url) {
     swal({
-        title: "Are you sure you want to Delete?",
+        title: "Are you sure you want to delete?",
         text: "You will not be able to restore the data!",
         icon: "warning",
         buttons: true,
